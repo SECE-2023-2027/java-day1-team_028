f3351d4d83404d6bb72c55fa6a2d8adb9001fc8b
Anisha Dharshini <anisha12dharshini@gmail.com>
2026-06-16 09:37:51 +0530

    Program 10

--- Program_10.java
+++ Program_10.java
@@ -1,3 +1,17 @@
+import java.util.Scanner;
+
 public class Program_10 {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input a string: ");
+        String str = sc.nextLine();
+
+        String rev = "";
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            rev += str.charAt(i);
+        }
+
+        System.out.println("Reverse string: " + rev);
+    }
 }
